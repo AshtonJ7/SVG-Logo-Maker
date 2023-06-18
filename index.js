9788aee736879a633699a30ca5150b6acc702426
AshtonJ7 <AshtonJ7@users.noreply.github.com>
2023-06-18 03:45:51 +0100

    questions added to command line and code for tests inputed

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-// music should be an object with title, artist, and album properties
-const music = {
-    // code here
-   title: "happiness",
-   artist: "1975",
-   album: "Being funny in a foreign language"
-  };
-  
-  // Write code between the backticks tags to output the data from the music object above.
-  const songSnippet = `My favourite song is ${music.title} by ${music.artist} on the ${music.album}!`;
-  
-  console.log(songSnippet);
-  
+const CLI = require('./lib/cli');
+
+const cli = new CLI();
+
+cli.run();
